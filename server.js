2bf3152cc76de4da163934202c7f8e202dddd685
oabrivard <olivier@abrivard.fr>
2013-02-19 17:16:17 +0100

    adding IP address detection

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+function getClientAddress(request){ 
+    with(request)
+        return (headers['x-forwarded-for'] || '').split(',')[0] 
+            || connection.remoteAddress
+}
+
+
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Azure\n');
+  res.write('Hello !\n');
+  res.write('Your IP Address is : ' + getClientAddress(req));
+  res.end('\n');
 }).listen(port);
